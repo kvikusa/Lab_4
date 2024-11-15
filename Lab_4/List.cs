6d39c92dea17ac5095c19f1e21439f8588de43e4
Виктория <@KOMPUTER>
2024-11-15 11:46:41 +0500

    Добавьте файлы проекта.

---
+++ Lab_4/List.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security;
+
+namespace Lab_4
+{
+    public class ListTask
+    {
+        //поля
+        //аксессоры(свойства)
+        //Конструкторы
+
+        //методы
+        //заполнение списка случайными числами
+        public static void ListInt<T>(int n, List <string> L) 
+        {
+            for (int i = 0; i < n; i++)
+            {
+                L.Add(Console.ReadLine());
+            }
+        }
+
+        //вывод списка на экран
+        public static void PrintList<T> (List<string> list)
+        {
+            foreach (string str in list)
+            {
+                Console.Write(str + " ");
+            }
+            Console.WriteLine();
+        }
+        //формирует список L, включив в него по одному разу элементы, которые входят в список L1, но не входят в список L2
+        public static List<T> Task1<T>(List<T> L1, List<T> L2)
+        {
+            return L1.Except(L2).ToList();
+        }
+
+        //проверяет симметричность списка
+        public static bool Symmetric(LinkedList<int> list, int i, int j)
+        {
+            var first = list.First;
+            var last = list.Last;
+
+            for (int k = 0; k <= i; k++) //присваеваем i-му эл-ту значение
+                first = first.Next;
+
+            for (int k = 0; k <= list.Count - 1 - j; k++)
+                last = last.Previous;
+
+            while (first != last && first.Previous != last)
+            {
+                if (first.Value != last.Value)
+                    return false; // Если элементы не равны, возвращаем false
+
+                first = first.Next; //присваеваем следущее значение
+                last = last.Previous; //присваеваем следущее значение с конца
+            }
+            return true; // Если все элементы равны, возвращаем true
+        }
+        public static void PrintListLink(LinkedList<int> list)
+        {
+            foreach (int n in list)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void VvodLinkList(LinkedList<int> l)
+        {
+            while (true)
+            {
+                Console.Write("Введите число или 'stop': ");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "stop")
+                    break;
+
+                if (int.TryParse(input, out int number))
+                {
+                    l.AddLast(number);
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число или 'stop'.");
+                }
+            }
+        }
+
+        //Есть перечень названий шоколада. Определить для каждого наименования шоколада, какие из них нравятся всем n сладкоежкам, какие — некоторым из сладкоежек, и какие — никому из сладкоежек.
+        public static void HashSet()
+        {
+            //Создаем HashSet для каждого покупателя
+            HashSet<string> sweetTooth1 = new HashSet<string> { "Alpen Gold", "Milka", "Nesquik", "Ritter sport" };
+            HashSet<string> sweetTooth2 = new HashSet<string> { "Ritter sport", "Kinder" };
+            HashSet<string> sweetTooth3 = new HashSet<string> { "Alpen Gold", "Ritter sport", "Kinder" } ;
+            HashSet<string> sweetTooth4 = new HashSet<string> { "Ritter sport", "Milka" };
+
+            //Создаем HashSet для каждого шоколада
+            HashSet<string> chocolate1 = new HashSet<string> { "Alpen Gold" };
+            HashSet<string> chocolate2 = new HashSet<string> { "Ritter sport" };
+            HashSet<string> chocolate3 = new HashSet<string> { "Milka" };
+            HashSet<string> chocolate4 = new HashSet<string> { "Nesquik" };
+            HashSet<string> chocolate5 = new HashSet<string> { "Kinder" };
+            HashSet<string> chocolate6 = new HashSet<string> { "Dove" };
+
+            //Определяем какой шоколад нравится всем n сладкоежкам
+            HashSet<string> all_buyers = new HashSet<string>(sweetTooth1);
+            all_buyers.IntersectWith(sweetTooth2);
+            all_buyers.IntersectWith(sweetTooth3);
+            all_buyers.IntersectWith(sweetTooth4);
+
+
+            //Определяем какой шоколад приобретался некоторыми из покупателей сладкоежек
+            HashSet<string> some_buyers = new HashSet<string>(sweetTooth1);
+            some_buyers.UnionWith(sweetTooth2);
+            some_buyers.UnionWith(sweetTooth3);
+            some_buyers.UnionWith(sweetTooth4);
+
+            //Определяем какой шоколад не приобретался ни одним из покупателей
+            HashSet<string> null_choc = new HashSet<string>(chocolate1);
+            null_choc.UnionWith(chocolate2);
+            null_choc.UnionWith(chocolate3);
+            null_choc.UnionWith(chocolate4);
+            null_choc.UnionWith(chocolate5);
+            null_choc.UnionWith(chocolate6);
+            null_choc.ExceptWith(some_buyers);
+    
+            //Исключаем шоколад который покупали все
+            some_buyers.ExceptWith(all_buyers);
+
+            Console.Write("\nШоколад, который нравится всем сладкоежкам -> ");
+            foreach (string chocolate in all_buyers)
+                Console.WriteLine(chocolate);
+
+            Console.WriteLine("\nШоколад, который нравится некоторым из сладкоежек:");
+            foreach (string chocolate in some_buyers)
+                Console.WriteLine(chocolate);
+
+            Console.Write("\nШоколад, который не нравится никому из сладкоежек ->  ");
+            foreach (string chocolate in null_choc)
+                Console.WriteLine(chocolate);
+        }
+
+        public static void Bukv (string file)
+        {
+            // Полный набор букв русского алфавита (включая заглавные и строчные)
+            HashSet<char> rusAlphabet = new HashSet<char>
+        {
+            'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м',
+            'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ',
+            'ы', 'ь', 'э', 'ю', 'я',
+            'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М',
+            'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ',
+            'Ы', 'Ь', 'Э', 'Ю', 'Я'
+        };
+
+            // Создаем HashSet для хранения найденных букв
+            HashSet<char> foundBykv = new HashSet<char>();
+
+            try
+            {
+                string text = File.ReadAllText(file);
+
+                // Проходим по каждому символу в тексте
+                foreach (char c in text)
+                {
+                    // Если символ — буква русского алфавита, добавляем его в HashSet
+                    if (rusAlphabet.Contains(char.ToLower(c))) // Приводим к нижнему регистру для унификации
+                    {
+                        foundBykv.Add(char.ToLower(c));
+                    }
+                }
+
+                // Находим буквы, которые не встречаются в тексте
+                rusAlphabet.ExceptWith(foundBykv);
+
+                // Выводим результат
+                Console.WriteLine($"Количество букв, которые не встречаются: {rusAlphabet.Count}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка при чтении файла.");
+            }
+        }
+    }
+}
---
+++ Lab_4/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Практика_3;
+
+namespace Lab_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int z = Proverka.InputIntegerWithValidation($"Введите номер задачи (от 1 до 4) -> ", 1, 4); ;
+            if (z == 1)
+            {
+                List<string> L1 = new List<string>();
+                int n;
+                while (true)
+                {
+                    Console.Write("Введите размер списка L1 -> ");
+                    if (int.TryParse(Console.ReadLine(), out n))
+                        break;
+                    else
+                        Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число размерa списка L1 ->");
+                }
+                ListTask.ListInt<string>(n, L1);
+                Console.Write("L1 -> ");
+                ListTask.PrintList<string>(L1);
+
+                List<string> L2 = new List<string>();
+                int m;
+                while (true)
+                {
+                    Console.Write("Введите размер списка L2 -> ");
+                    if (int.TryParse(Console.ReadLine(), out m))
+                        break;
+                    else
+                        Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число размерa списка L2 ->");
+                }
+                ListTask.ListInt<string>(m, L2);
+                Console.Write("L2 -> ");
+                ListTask.PrintList<string>(L2);
+
+                List<string> L = ListTask.Task1(L1, L2);
+                Console.Write("L -> ");
+                ListTask.PrintList<string>(L);
+            }
+            if (z == 2) 
+            {
+                LinkedList<int> list = new LinkedList<int>();
+                Console.WriteLine("Введите целые числа для заполнения списка. Введите 'stop' для завершения ввода.");
+                ListTask.VvodLinkList(list);
+
+                if (list.Count == 0)
+                {
+                    Console.WriteLine("Список пуст. Программа завершена.");
+                    return;
+                }
+
+                Console.WriteLine("список: " );
+                ListTask.PrintListLink(list);
+
+                int i, j;
+                while (true)
+                {
+                    Console.Write("Введите индекс i (от 0 до {0}): ", list.Count-1);
+                    if (int.TryParse(Console.ReadLine(), out i) && i >= 0 && i < list.Count-1)
+                        break;
+                    else
+                        Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число в указанном диапазоне.");
+                }
+
+                while (true)
+                {
+                    Console.Write("Введите индекс j (от {0} до {1}): ", i + 1, list.Count-1);
+                    if (int.TryParse(Console.ReadLine(), out j) && j > i && j < list.Count)
+                        break;
+                    else
+                        Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число в указанном диапазоне.");
+                }
+                
+                bool sim = ListTask.Symmetric(list, i, j);
+                Console.WriteLine($"Участок списка с {i} по {j} является симметричным: {sim}");
+            }
+            if (z == 3)
+            {
+                ListTask.HashSet();
+            }
+            if (z == 4) 
+            { 
+                string fileName = "textEng.txt"; //чтение файла
+                ListTask.Bukv(fileName);
+            }
+        }
+    }
+}
